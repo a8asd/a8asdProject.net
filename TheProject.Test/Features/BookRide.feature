f34031da7885db69dee3ceac0f844b1841a035d7
Derek Lewies <derek.lewies@rwe.com>
2019-05-21 16:17:10 +0100

    Our first stepflow test

---
+++ TheProject.Test/Features/BookRide.feature
@@ -0,0 +1,11 @@
+﻿Feature: BookRide
+	In order to book a ride from a to b
+	As a Pat 
+	I want to Charlie to be available
+
+@mytag
+Scenario: Pat books a ride with Charlie
+	Given Pat is a registered customer
+	And Charlie is a available driver
+	When Pat books a ride
+	Then Charlie is Pat's driver
---
+++ TheProject.Test/Features/BookRideSteps.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Data.SqlTypes;
+using NUnit.Core;
+using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace TheProject.Test.Features
+{
+    [Binding]
+    public class BookRideSteps
+    {
+        private Customer pat;
+        private Driver charlie;
+        private Booking booking;
+
+        [Given(@"(.*) is a registered customer")]
+        public void GivenPatIsARegisteredCustomer(string name)
+        {
+           pat = new Customer {Name = name};
+        }
+
+        [Given(@"(.*) is a available driver")]
+        public void GivenCharlieIsAAvailableDriver(string name)
+        {
+            charlie = new Driver{Name = name};
+    }
+        
+        [When(@"Pat books a ride")]
+        public void WhenPatBooksARide()
+        {
+           booking = new Booking();
+           booking.Customer = pat;
+           booking.Driver = charlie;
+        }
+        
+        [Then(@"Charlie is Pat's driver")]
+        public void ThenCharlieIsPatSDriver()
+        {
+            Assert.AreEqual(pat, booking.Customer);
+            Assert.AreEqual(charlie, booking.Driver);
+        }
+    }
+
+    public class Booking
+    {
+        public Driver Driver { get; set; }
+        public Customer Customer { get; set; }
+    }
+
+    public class Driver
+    {
+        public string Name { get; set; }
+    }
+
+    public class Customer
+    {
+        public string Name { get; set; }
+    }
+}
