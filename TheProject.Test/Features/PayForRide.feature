1c89dc5a9771aa012badcbc441f76dfac1de92bf
mike challis <mike@challis.org>
2019-05-22 16:26:31 +0100

    Pay for ride

---
+++ TheProject.Test/Features/PayForRide.feature
@@ -0,0 +1,14 @@
+﻿Feature: PayForRide
+
+Background: There exists a booking 
+Given Pat is a registered customer
+And Charlie is an available driver
+When Pat books a ride with Charlie
+
+Scenario: Pat pays for a ride
+	Given Pat has traveled 10 miles with Charlie
+	And the rate is £2.00 per mile
+	When Pat pays for the ride
+	Then these invoices are in the system
+	| Payee | Driver  | Distance | Amount |
+	| Pat   | Charlie | 10.0     | 20.00  |
---
+++ TheProject.Test/Features/PayForRideSteps.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace TheProject.Test.Features
+{
+    [Binding]
+    public class PayForRideSteps
+    {
+        private List<InvoiceItem> invoiceItems = new List<InvoiceItem>();
+        private Ride ride = new Ride();
+
+        [Given(@"Pat has traveled (.*) miles with Charlie")]
+        public void GivenPatHasTraveledMilesWithCharlie(decimal distance)
+        {
+            Booking booking = new Booking
+                {Customer = new Customer {Name = "Pat"}, Driver = new Driver {Name = "Charlie"}};
+            ride.Booking = booking;
+            ride.Distance = distance;
+        }
+        
+        [Given(@"the rate is £(.*) per mile")]
+        public void GivenTheRateIsPerMile(Decimal rate)
+        {
+            ride.Rate = rate;
+        }
+        
+        [When(@"Pat pays for the ride")]
+        public void WhenPatPaysForTheRide()
+        {
+            invoiceItems.Add(ride.Pay());
+        }
+        
+        [Then(@"these invoices are in the system")]
+        public void ThenTheseInvoicesAreInTheSystem(Table table)
+        {
+           table.CompareToSet(invoiceItems);
+        }
+    }
+
+    internal class Ride
+    {
+        internal decimal Distance;
+        internal Booking Booking;
+        internal decimal Rate;
+
+        internal InvoiceItem Pay()
+        {
+            InvoiceItem invoiceItem = new InvoiceItem();
+            invoiceItem.Payee = Booking.Customer.Name;
+            invoiceItem.Driver = Booking.Driver.Name;
+            invoiceItem.Distance = this.Distance;
+            invoiceItem.Amount = this.Distance * this.Rate;
+
+            return invoiceItem;
+        }
+    }
+    
+    internal class InvoiceItem
+    {
+        public string Payee { get; set; }
+        public string Driver { get; set; }
+
+        public decimal Distance { get; set; }
+
+        public decimal Amount { get; set; }
+    }
+}
