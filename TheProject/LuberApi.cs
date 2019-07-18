e9ac70dd3c8939e7a1b43539c24c0cfd91330ae7
davidputman <davidputman@davidputman.com>
2019-07-18 13:56:13 +0100

    book ride

--- TheProject/LuberApi.cs
+++ TheProject/LuberApi.cs
@@ -4,6 +4,7 @@ namespace TheProject
 {
     public class LuberApi
     {
+        private const int MaximumDistance = 30;
         private readonly List<Driver> drivers = new List<Driver>();
 
         public void AddDriver(Driver driver)
@@ -16,17 +17,27 @@ namespace TheProject
             IList<DriverLocation> driverLocations = new List<DriverLocation>();
             foreach (var driver in drivers)
             {
-                if (driver.Location.Distance(customerLocation) <= 30)
-                {
-                    driverLocations.Add(
-                        new DriverLocation
-                        {
-                            Name = driver.Name,
-                            TimeToPickup = driver.TimeToPickup(customerLocation)
-                        });
-                }
+                AddDriverToListIfCloseEnough(customerLocation, driver, driverLocations);
             }
             return driverLocations;
+        }
+
+        private static void AddDriverToListIfCloseEnough(Location customerLocation, Driver driver, IList<DriverLocation> driverLocations)
+        {
+            if (IsDriverCloseEnough(customerLocation, driver))
+            {
+                driverLocations.Add(
+                    new DriverLocation
+                    {
+                        Name = driver.Name,
+                        TimeToPickup = driver.TimeToPickup(customerLocation)
+                    });
+            }
+        }
+
+        private static bool IsDriverCloseEnough(Location customerLocation, Driver driver)
+        {
+            return driver.Location.Distance(customerLocation) <= MaximumDistance;
         }
     }
 }
