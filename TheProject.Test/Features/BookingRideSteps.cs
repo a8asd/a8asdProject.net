e8c1c1c6d0e87fc5dafeae1a6a9ba0ba34debd36
Paul Johnson <paul.johnson@prospectsoft.com>
2019-11-06 12:59:29 +0000

    Fixed some code smells in Riley takes a ride

--- TheProject.Test/Features/BookingRideSteps.cs
+++ TheProject.Test/Features/BookingRideSteps.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Net.Sockets;
 using TechTalk.SpecFlow;
 using TechTalk.SpecFlow.Assist;
 using TheProject.Models;
@@ -11,44 +9,41 @@ namespace TheProject.Test.Features
     [Binding]
     public class BookingRidesSteps
     {
-        private List<Driver> driverList = new List<Driver>();
+        private readonly List<Driver> driverList = new List<Driver>();
         private List<Driver> availableDrivers;
-        private List<Member> memberList = new List<Member>();
+        private readonly List<Member> memberList = new List<Member>();
 
         [Given(@"(.*) is a member at (.*),(.*)")]
-        public void GivenRileyIsAMember(string memberName,double latitude, double longitude)
+        public void GivenRileyIsAMember(string memberName, double latitude, double longitude)
         {
-            memberList.Add(new Member{Name = memberName,Location = new Location(latitude,longitude)});
-        } 
-        
-        [Given(@"(.*) is a driver at (.*),(.*)")]
-        public void GivenDannyIsADriverAt(string driverName,double latitude, double longitude)
-        {
-            driverList.Add(new Driver {Name = driverName,Location = new Location(latitude,longitude)});
+            memberList.Add(new Member { Name = memberName, Location = new Location(latitude, longitude) });
         }
-        
-        [When(@"(.*) requests a ride from (.*),(.*)")]
-        public void WhenRileyRequestsARideFrom(string memberName,double latitude, double longitude)
+
+        [Given(@"(.*) is a driver at (.*),(.*)")]
+        public void GivenDannyIsADriverAt(string driverName, double latitude, double longitude)
         {
-            
-            if (memberList.Find(x=>x.Name==memberName)!=null)
+            driverList.Add(new Driver { Name = driverName, Location = new Location(latitude, longitude) });
+        }
+
+        [When(@"(.*) requests a ride from (.*),(.*)")]
+        public void WhenRileyRequestsARideFrom(string memberName, double latitude, double longitude)
+        {
+
+            if (memberList.Find(x => x.Name == memberName) != null)
             {
                 Location memberLocation = new Location(latitude, longitude);
 
                 //availableDrivers = driverList.FindAll(d => d.Location.DistanceFrom(memberLocation) <= 16.0);
-                var sortedDriverlist = (from driver in driverList
-                        where driver.Location.DistanceFrom(memberLocation) <= 16.0
-                        select (distance: driver.Location.DistanceFrom(memberLocation), driver: driver))
+                var sortedDriverList = (from driver in driverList
+                                        where driver.Location.DistanceFrom(memberLocation) <= 16.0
+                                        select (distance: driver.Location.DistanceFrom(memberLocation), driver))
                     .OrderBy(x => x.distance);
-                    if(sortedDriverlist.Count() > 5)             
-                        availableDrivers = sortedDriverlist.Select(x => x.driver).ToList().GetRange(0,5);
-                    else
-                    {
-                        availableDrivers = sortedDriverlist.Select(x => x.driver).ToList();
-                    }
+                availableDrivers = sortedDriverList.Select(x => x.driver).ToList();
+                if (availableDrivers.Count() > 5)
+                    availableDrivers = availableDrivers.GetRange(0, 5);
             }
         }
-        
+
         [Then(@"Riley sees these drivers")]
         public void ThenRileySeesTheseDrivers(Table table)
         {
